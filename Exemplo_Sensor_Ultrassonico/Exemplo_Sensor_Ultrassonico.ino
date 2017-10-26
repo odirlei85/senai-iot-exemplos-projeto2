a9b55ce76a93411c7b4feb3a8e2e9c8f281ad1bc
ODIRLEI SABELLA DE ASSIS <32184284831@jpmorgan.sp>
2017-10-26 21:08:32 -0200

    teste

--- Exemplo_Sensor_Ultrassonico/Exemplo_Sensor_Ultrassonico.ino
+++ Exemplo_Sensor_Ultrassonico/Exemplo_Sensor_Ultrassonico.ino
@@ -1,15 +1,20 @@
 // Inclusão da biblioteca do sensor
-// Essa biblioteca não vem por padrão com o arduino e 
+// Essa biblioteca não vem por padrão com o arduino e
 // precisa ser instalada pelo gerenciador de bibliotecas
 #include <Ultrasonic.h>
 
 // Inicializa o objeto do sensor ultrasônico
 // Usando as portas 12 e 13 para trigger e echo
 Ultrasonic ultrasonic(12, 13);
+const int ledVermelho = 11;
+const int ledAmarelo = 10;
 
 void setup() {
   // Inicializa a porta Serial
   Serial.begin(9600);
+  pinMode (ledVermelho, OUTPUT);
+  pinMode (ledAmarelo, OUTPUT);
+
 }
 
 void loop() {
@@ -19,6 +24,13 @@ void loop() {
   // Escreve o valor da distância no painel Serial
   Serial.print("Distance in CM: ");
   Serial.println(distancia);
-  
+
   delay(1000);
-}
+
+  if (distancia <= 10) {
+    digitalWrite(ledVermelho, HIGH);
+  }
+  else {
+    digitalWrite(ledVermelho, LOW);
+    }
+  }
